58e30b418e22c203ee2f859fe213ac160c2753d7
Vyacheslav Kartashov <ynksdk1654@gmail.com>
2025-07-07 16:01:00 +0300

    feat: added favicon

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,25 @@
 <script setup lang="ts">
+import { appDescription, appName } from './constants/head'
+
 const layout = 'main-layout'
+
+useHead({
+	title: appName,
+	meta: [
+		{ name: 'description', content: appDescription },
+		{ name: 'apple-mobile-web-app-title', content: appName },
+	],
+	htmlAttrs: {
+		lang: 'ru',
+	},
+	link: [
+		{ rel: 'manifest', type: 'application/json', href: '/manifest.webmanifest' },
+		{ rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+		{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+		{ rel: 'shortcut icon', href: '/favicon.ico' },
+		{ rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+	],
+})
 </script>
 
 <template>
@@ -8,6 +28,6 @@ const layout = 'main-layout'
 	</NuxtLayout>
 </template>
 
-<style lang="scss" module>
+<style lang="scss">
 @use '~/assets/scss/_main.scss' as *;
 </style>
---
+++ src/constants/head.ts
@@ -0,0 +1,2 @@
+export const appName = 'GlovBox'
+export const appDescription = 'The best sports equipment'
