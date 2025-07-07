375595a42700162ce3b9247b1bfef8bb6c5444f4
Vyacheslav Kartashov <ynksdk1654@gmail.com>
2025-07-07 16:47:43 +0300

    chore: improted all sections

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
+import AdditionalInfo from '@/components/section/AdditionalInfo.vue'
+import Benefits from '@/components/section/Benefits/Benefits.vue'
+import ChooseUs from '@/components/section/ChooseUs/ChooseUs.vue'
+import ContactSection from '@/components/section/Contact/ContactSection.vue'
+import Faq from '@/components/section/Faq/Faq.vue'
+import Goods from '@/components/section/Goods/Goods.vue'
+import Hero2 from '@/components/section/Hero2.vue'
+import Video from '@/components/section/Video.vue'
+
 definePageMeta({
 	layout: 'main-layout',
 })
 </script>
 
 <template>
-	<div>HomePage</div>
+	<div>
+		<Hero2 />
+		<AdditionalInfo />
+		<ChooseUs />
+		<Video />
+		<Goods />
+		<Benefits />
+		<Faq />
+		<ContactSection />
+	</div>
 </template>
