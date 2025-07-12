031c5519d70bb421675e662d3fa822742d0b4716
Vyacheslav Kartashov <ynksdk1654@gmail.com>
2025-07-12 22:56:40 +0300

    refactor: deleted unused logic and styles in NavbarButton

---
+++ src/components/header/NavbarButton.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+const { t } = useI18n()
+
+function handleClick() {
+	const target = document.getElementById('contact') as HTMLElement
+	target.scrollIntoView({ behavior: 'smooth' })
+}
+</script>
+
+<template>
+	<button
+		:class="$style.navbarButton"
+		@click="handleClick"
+	>
+		{{ t("button.toOrder") }}
+	</button>
+</template>
+
+<style lang="scss" module>
+.navbarButton {
+	display: inline-flex;
+	align-items: center;
+	justify-content: center;
+
+	width: 15vh;
+	height: 2.25rem;
+	padding: 0.5rem 1rem;
+
+	cursor: pointer;
+	transition:
+		background-color 0.2s ease,
+		color 0.2s ease;
+	animation: smoothPulse 1.5s ease-in-out infinite;
+
+	background-color: var(--color-primary);
+	color: var(--text-button);
+	font-family: 'Montserrat';
+	font-size: 0.875rem;
+	font-weight: 800;
+	text-transform: uppercase;
+	letter-spacing: 0.5px;
+	text-decoration: none;
+	line-height: 1;
+	border: none;
+	user-select: none;
+
+	&:hover {
+		background-color: var(--color-primary-lighter);
+		box-shadow: 0px 0px 15px 1px var(--color-primary-lighter);
+	}
+
+	&:active {
+		background-color: var(--color-primary-darker);
+	}
+
+	@media (max-width: 768px) {
+		margin-left: auto;
+	}
+}
+
+@keyframes smoothPulse {
+	0%,
+	100% {
+		box-shadow: 0 0 25px 5px var(--color-primary-lighter);
+	}
+
+	50% {
+		box-shadow: 0 0 25px 1px var(--color-primary-lighter);
+	}
+}
+</style>
