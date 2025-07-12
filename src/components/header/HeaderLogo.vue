f8d23b3fb0a6c0ca62368944d372552ed4937aef
Vyacheslav Kartashov <ynksdk1654@gmail.com>
2025-07-12 22:57:08 +0300

    refactor: brought out logic of logo in header

---
+++ src/components/header/HeaderLogo.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import IconLogo from '../ui/icons/IconLogo.vue'
+</script>
+
+<template>
+	<a
+		:class="$style.logoLink"
+		href="/"
+	>
+		<IconLogo
+			:class="$style.logoLink"
+			:width="120"
+		/>
+	</a>
+</template>
+
+<style lang="scss" module>
+.logoLink {
+	display: block;
+
+	cursor: pointer;
+
+	user-select: none;
+	-webkit-user-select: none;
+	-moz-user-select: none;
+	-ms-user-select: none;
+
+	color: var(--text-primary);
+}
+</style>
