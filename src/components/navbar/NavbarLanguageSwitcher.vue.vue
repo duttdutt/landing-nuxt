eb8757a8f06ee18de58f954d14728ed83b732e77
Vyacheslav Kartashov <ynksdk1654@gmail.com>
2025-07-12 06:14:21 +0300

    feat: create language switcher

---
+++ src/components/navbar/NavbarLanguageSwitcher.vue.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRouter } from 'vue-router'
+import IconEn from '@/components/ui/icons/IconEn.vue'
+import IconRu from '@/components/ui/icons/IconRu.vue'
+
+const { locale } = useI18n()
+
+const router = useRouter()
+const switchLocalePath = useSwitchLocalePath()
+
+const isRu = computed(() => locale.value.startsWith('ru'))
+const currentIcon = computed(() => (isRu.value ? IconRu : IconEn))
+const nextCode = computed(() => (isRu.value ? 'en' : 'ru'))
+
+function toggleLocale() {
+	router.push(switchLocalePath(nextCode.value))
+}
+</script>
+
+<template>
+	<button :class="$style.languageToggle" @click="toggleLocale">
+		<component :is="currentIcon" />
+	</button>
+</template>
+
+<style module lang="scss">
+@use '@/assets/scss/_variables' as *;
+
+.languageToggle {
+	display: inline-flex;
+	align-items: center;
+	justify-content: center;
+
+	width: 2.3rem;
+	height: 2.3rem;
+
+	cursor: pointer;
+
+	transition: background 0.3s ease;
+
+	background-color: var(--bg-primary);
+	border: 2px solid transparent;
+	color: var(--text-primary);
+}
+</style>
